07f8eb0d0a831a6a3172fa361bf4890a2576a6a0
ccaleanu <catalin.caleanu@upt.ro>
2021-03-21 10:59:14 +0200

    densenet121 added

--- model/classic/__init__.py
+++ model/classic/__init__.py
@@ -4,4 +4,4 @@ from .ResNet50 import ResNet50
 from .MobileNetV2 import MobileNetV2
 from .NASNetMobile import NASNetMobile
 from .EfficientNetB0 import EfficientNetB0
-#from .DenseNet121 import DenseNet121
+from .DenseNet121 import DenseNet121
