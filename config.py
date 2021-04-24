030bd2532e038bc9e1320a158617e0eeea49e05c
ccaleanu <catalin.caleanu@upt.ro>
2021-04-24 08:32:26 +0300

    keep config

--- config.py
+++ config.py
@@ -5,16 +5,16 @@
 data_dir = 'e:/MY/My Databases/MEDICAL/CEUS/UMF/DBV50LEZIUNI'
 
 #PATIENS_TAKEN = {False = all patients, number}
-#PATIENS_TAKEN = False
-PATIENS_TAKEN = 1
+PATIENS_TAKEN = False
+#PATIENS_TAKEN = 1
 
-EPOCHS = 12
+EPOCHS = 40
 #Early Stopping patience
 #patience = EPOCHS/2
-patience = 5
+patience = 15
 
-SHUFFLE = True
-#SHUFFLE = False
+#SHUFFLE = True
+SHUFFLE = False
 
 EXPERIMENTS = 1
 
@@ -25,38 +25,40 @@ TF_Board = False
 # classic myModelName: any from https://keras.io/api/applications/, e.g., ResNet50, MobileNetV2, etc.
 # custom myModelName: {SequentialS, Sequential12, Sequential17}
 
-#myModelType = 'model.classic'
+myModelType = 'model.classic'
 #myModelName = 'MobileNetV2'
-#myModelName = 'NASNetMobile'
+myModelName = 'NASNetMobile'
 #myModelName ='EfficientNetB0'
 #myModelName ='DenseNet121'
 #myModelName = 'ResNet50'
 #myModelName = 'EfficientNetB3'
 
-myModelType = 'model.custom'
+#myModelType = 'model.custom'
 #myModelName = 'SequentialS'
-myModelName = 'Sequential12'
+#myModelName = 'Sequential12'
 #myModelName = 'Sequential17'
 
 batch_size = 16
 
 #color_mode must be one of {"rbg", "rgba", "grayscale"}
-color_mode = 'grayscale'
-#color_mode = 'rgb'
+#color_mode = 'grayscale'
+color_mode = 'rgb'
 #depth must be one of {1 - for grayscale, 3 - for rgb and rgba}
-depth = 1
-#depth = 3
+#depth = 1
+depth = 3
 
 #settings for all custom Sequentials
-img_height = 180
-img_width = 180
+#img_height = 180
+#img_width = 180
+
+
 
 #settings for all Classics
-#img_height = 224
-#img_width = 224
+img_height = 224
+img_width = 224
 #weights={'imagenet', None}
-#weights=None
+weights=None
 #trainable = {True, False}
-#trainable = True
-#AUG = True
-#PREPROC = True
+trainable = True
+AUG = True
+PREPROC = True
